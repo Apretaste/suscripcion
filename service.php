26f7e292495cc110e936152975e9cf771cccfc80
salvipascual <salvi.pascual@gmail.com>
2016-12-11 21:02:47 -0500

    finish service

--- service.php
+++ service.php
@@ -3,27 +3,75 @@
 class Suscripcion extends Service
 {
 	/**
-	 * Function executed when the service is called
+	 * Display the status of your suscriptions
 	 *
 	 * @param Request
 	 * @return Response
 	 * */
 	public function _main(Request $request)
 	{
-		/*
-		// make person inactive
+		// get the status of the mail list
 		$connection = new Connection();
-		$sql = "UPDATE person SET active=0 WHERE email='{$request->email}'";
-		$connection->deepQuery($sql);
+		$status = $connection->deepQuery("SELECT mail_list FROM person WHERE email = '{$request->email}'");
+		$status = array("maillist"=>$status[0]->mail_list);
 
-		// unsubscribe from the mailing list in Mailer Lite
-		$utils = new Utils();
-		$utils->unsubscribeFromEmailList($request->email);
-*/
 		// create response
 		$response = new Response();
 		$response->setResponseSubject("Usted ha sido excluido de Apretaste");
-		$response->createFromTemplate("basic.tpl", array());
+		$response->createFromTemplate("basic.tpl", $status);
+		return $response;
+	}
+
+	/**
+	 * Start your suscription to the email list
+	 *
+	 * @param Request
+	 * @return Response
+	 * */
+	public function _lista(Request $request)
+	{
+		// default case
+		$text = 'Su estado en la lista de correo no ha cambiado. Envie un email con asunto "SUSCRIPCION LISTA salir" para salr de la lista o "SUSCRIPCION LISTA entrar" para formar parte.';
+
+		// for the case unsubscribing to the list
+		if($request->query == "salir")
+		{
+			$this->utils->unsubscribeFromEmailList($request->email);
+			$text = 'Le hemos eliminado de la lista de correo. Ahora no recibira mas nuestra correspondencia';
+
+		}
+
+		// for the case subscribing to the list
+		if($request->query == "entrar")
+		{
+			$this->utils->subscribeToEmailList($request->email);
+			$text = 'Le hemos agregado a nuestra lista de correo. Ahora debera empezar a recibir nuestra correspondencia';
+		}
+
+		// create response
+		$response = new Response();
+		$response->setResponseSubject("Estado de la lista de correo");
+		$response->createFromText($text);
+		return $response;
+	}
+
+	/**
+	 * Exlude a user from Apretaste and close his/her account
+	 *
+	 * @param Request
+	 * @return Response
+	 * */
+	public function _excluyeme(Request $request)
+	{
+		// make person inactive
+		$connection = new Connection();
+		$connection->deepQuery("UPDATE person SET active=0 WHERE email='{$request->email}'");
+
+		// create response
+		$response = new Response();
+		$response->setEmailLayout("email_simple.tpl");
+		$response->setResponseSubject("Hemos cerrado su cuenta");
+		$response->createFromText("<b>Hemos cerrado su cuenta de Apretaste.</b> Usted no recibira ningun otro email de nosotros. Tampoco mostraremos su perfil a otros usuarios. Si quiere reabrir su cuenta, utilice Apretaste nuevamente y sera abierta de manera automatica.");
 		return $response;
 	}
 }
--- templates/basic.tpl
+++ templates/basic.tpl
@@ -1,6 +1,20 @@
 <h1>Estado de sus suscripciones</h1>
 <p>A continuaci&oacute;n la lista de suscripciones en Apretaste y su estado.</p>
+
 {space10}
+
 <h2>Lista de correo</h2>
-<p><small>Si est&aacute; suscrito a la lista de correos le dejaremos saber de nuevos servicios que saquemos, invitaciones a participar en nuestra Rifa e informaci&oacute;n sobre los  ganadores de nuestra Rifa y concursos, entre otras.</small></p>
-<p>Estado: <font color="green">SUSCRITO</font> {button href="" caption="Desuscribirse" size="small" color="red"}</p>
+<p><small>Si est&aacute; suscrito a la lista de correos le dejaremos saber de nuevos servicios que saquemos, le enviaremos invitaciones a participar en nuestra rifa e informaci&oacute;n sobre los  ganadores de nuestras rifa y concursos, entre otras.</small></p>
+<p>Estado:
+	{if $maillist}
+		<font color="green">SUSCRITO</font> {button href="SUSCRIPCION LISTA salir" caption="Desuscribirse" size="small" color="red"}
+	{else}
+		<font color="red">UNSUSCRITO</font> {button href="SUSCRIPCION LISTA entrar" caption="Suscribirse" size="small" color="green"}
+	{/if}
+</p>
+
+{space10}
+
+<h2>Apretaste</h2>
+<p><small>Si no deseas usar m&aacute;s Apretaste puedes cerrar tu cuenta por esta v&iacute;a. <b>Una vez que cierre su cuenta no recibira ning&uacute;n email de nosotros.</b> Si usas Apretaste en un futuro tu cuenta se volver&aacute; a abrir autom&aacute;ticamente.</small></p>
+{button href="SUSCRIPCION EXCLUYEME" caption="Cerrar cuenta" size="small" color="red"}</p>
